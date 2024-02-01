8bbc97306ee7bcd5fe096854dad5cd22bc4cbb95
Samuel Kim <41707531+samuel129@users.noreply.github.com>
2024-01-31 17:46:00 -0800

    Update cecs328pa1.py

--- cecs328pa1.py
+++ cecs328pa1.py
@@ -4,9 +4,26 @@ import sys
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
-        return file.read()
-
+        return [line.strip() for line in file]
+    
+def sumDigits(digits):
+    return sum(int(digit) for digit in digits)
+    
 file_name = sys.argv[1]
-content = read_file(file_name)
-print(content)
+numbers = read_file(file_name)
 
+foundNums = set()
+left = 0
+currMax = 0
+currSum = 0
+
+for num in range(len(numbers)):
+    total = sumDigits(numbers[num])
+    
+    while total in foundNums:
+        foundNums.remove(sumDigits(numbers[left]))
+        left += 1
+        currMax = max(currMax, (num - left + 1))
+        foundNums.add(total)
+
+print(currMax)
